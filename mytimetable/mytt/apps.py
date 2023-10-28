9aed005ee393c2ab807bdfaa03841d2a64f418c2
Harsayazheni <harsayazheni@gmail.com>
2023-10-28 13:25:56 +0000

    Success

--- mytimetable/mytt/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyttConfig(AppConfig):
-    name = 'mytt'
--- mytimetable/mytt/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def timetable(request):
-    return render(request,'mytimetable/timetable.html')
